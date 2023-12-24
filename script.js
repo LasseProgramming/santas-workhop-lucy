ed579bf4768d277317b4c6b7d885a5db0bdb79a1
Lasse Pedersen <lasse.r.p92@live.dk>
2023-12-24 16:19:38 +0100

    missing last clue

--- script.js
+++ script.js
@@ -30,10 +30,61 @@ const closeYellowClue = () => {
 }
 
 const openBlueClue = () => {
-    console.log("hello")
     document.getElementById('blue-clue-container').classList.remove('hidden');
 }
 
 const closeBlueClue = () => {
     document.getElementById('blue-clue-container').classList.add('hidden');
 }
+
+const openRedClue = () => {
+    document.getElementById('red-clue-container').classList.remove('hidden');
+}
+
+const closeRedClue = () => {
+    document.getElementById('red-clue-container').classList.add('hidden');
+}
+
+const openBlackClue = () => {
+    document.getElementById('black-clue-container').classList.remove('hidden');
+}
+
+const closeBlackClue = () => {
+    document.getElementById('black-clue-container').classList.add('hidden');
+}
+
+const openLastClue = () => {
+    document.getElementById('last-clue-container').classList.remove('hidden');
+}
+
+const closeLastClue = () => {
+    document.getElementById('last-clue-container').classList.add('hidden');
+}
+
+const incLock = (num) => {
+    const currentInner = parseInt(document.getElementById("lock" + num).innerHTML);
+    if (currentInner < 9) {
+        document.getElementById("lock" + num).innerHTML = currentInner + 1
+    }
+}
+
+const decLock = (num) => {
+    const currentInner = parseInt(document.getElementById("lock" + num).innerHTML);
+    if (currentInner > 0) {
+        document.getElementById("lock" + num).innerHTML = currentInner - 1
+    }
+}
+
+const checkLock = () => {
+    const result = parseInt(document.getElementById("lock" + 1).innerHTML +
+                    document.getElementById("lock" + 2).innerHTML +
+                    document.getElementById("lock" + 3).innerHTML +
+                    document.getElementById("lock" + 4).innerHTML 
+    )
+
+    if ( result == 1348) {
+        document.getElementById('black-clue-container').classList.add('hidden');
+        document.getElementById('last-clue-container').classList.remove('hidden');
+    }
+}
+
